a468e7e65bfc79c818e781f4205c29220cb599ff
mac <mac@macs-MacBook-Pro.local>
2024-07-23 12:43:59 +0800

    add property page starter

--- resources/views/ownerPage/tambahData.blade.php
+++ resources/views/ownerPage/tambahData.blade.php
@@ -1,5 +1,22 @@
 @extends('layout/main')
 @section('content')
     @include('components/navbar')
-    <h1>add propery for owner</h1>
+    
+    <div class="w-fullSize mx-auto font-poppins">
+        <h2 class="my-5 text-2xl font-semibold">Create Property</h2>
+
+        <section class="p-10 border border-slate-200 rounded-md">
+            <h1 class="text-lg">General Info</h1>
+
+            {{-- double input --}}
+            <div class="w-full flex justify-between">
+
+                <main>
+                    <label for="title">Name</label>
+                    <input type="text">
+                </main>
+
+            </div>
+        </section>
+    </div>
 @endsection
