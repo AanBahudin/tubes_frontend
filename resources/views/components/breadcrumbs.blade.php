22b459a1314fd128ea353d7f087168e293119ff0
Aan Bahudin <aan.bahudin11@gmail.com>
2024-07-25 00:56:24 +0800

    breadcrumbs value fix

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -1,6 +1,6 @@
 <div class="py-12">
     <main class="text-md font-poppins flex gap-x-5">
-        <a href="/" class="font-semibold hover:text-primary text-gray-400 ease-in-out duration-300 cursor-default">Home</a>
+        <a href="/" class="font-semibold hover:text-primary text-gray-400 ease-in-out duration-300 cursor-default">{{ $role == "USER" ? 'Home' : 'Dashboard' }}</a>
         <p  class="cursor-default">></p>
         <p class="cursor-default capitalize">{{ $params }}</p>
     </main>
