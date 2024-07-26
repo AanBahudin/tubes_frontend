7eb3560b5e19bd21544d21f853cbc1fe0d6958f6
Aan Bahudin <aan.bahudin11@gmail.com>
2024-07-26 22:21:41 +0800

    add conditional rendering in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,17 @@
     </div>
 
     {{-- CARD SECTION --}}
-    <div class="w-full md:w-fullSize mx-auto grid grid-cols-1 justify-items-center md:grid-cols-3 xl:grid-cols-4 gap-y-10 p-4">
-        @include('components/card', ['products' => $products])
-    </div>
+    
+
+    @if ($products->isNotEmpty())
+        <div class="w-full md:w-fullSize mx-auto grid grid-cols-1 justify-items-center md:grid-cols-3 xl:grid-cols-4 gap-y-10 p-4">
+            @include('components/card', ['products' => $products])
+        </div>
+    @else
+        <div class="font-poppins text-black font-semibold w-fullSize mx-auto p-4">
+            <h2 class="text-xl">No Results</h2>
+            <p class="text-md font-normal">Try changing or removing some of your filters.</p>
+            <a class="text-sm px-6 py-2 mt-5 block w-fit bg-primary text-white rounded-md hover:shadow-lg" href="/">Clear Filters</a>
+        </div>
+    @endif
 @endsection
